973e9682e8550edbbbb6eddc08c806e2f9c801c9
artanvrajolli <artanvrajolli@gmail.com>
2020-03-14 21:49:27 +0100

    new update
    add new error handling for numbers if is inserted

--- CryptoAlgorithms/tap-codeChiper.js
+++ CryptoAlgorithms/tap-codeChiper.js
@@ -41,15 +41,13 @@ module.exports = {
   tdecrypt: (plaintext) =>{
     plaintext = plaintext.trim();
     var decrypted_text ="";
-    if(plaintext.match(/[A-Za-z]/)){
+    if(plaintext.match(/[A-Za-z0-9]/)){
       console.log('Invalid syntax');
        process.exit();
    }
     var dotspliter = plaintext.split(/\s+/);
-   // console.log(dotspliter);
 
     var spacespliter = plaintext.split(/\.+/);
-   // console.log(spacespliter);
     var matrix = [['A','B','C','D','E'],['F','G','H','I','J']
     ,['L','M','N','O','P'],['Q','R','S','T','U']
     ,['V','W','X','Y','Z']];
@@ -74,8 +72,6 @@ module.exports = {
 
     }
     decrypted_text = decrypted_text.toLowerCase();
-   // console.log("dotslength:"+dotspliter.length+" spacelegth:"+spacespliter.length);
-
     console.log(decrypted_text);
   }
 }
