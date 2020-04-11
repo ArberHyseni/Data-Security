910c6108a76016b1254959cb62796a51460c7907
artanvrajolli <artanvrajolli@gmail.com>
2020-04-11 21:28:16 +0200

    Add files via upload

--- ModernEncryption/sendEncryptedMessage.js
+++ ModernEncryption/sendEncryptedMessage.js
@@ -4,11 +4,61 @@ const fs = require('fs')
 const os = require('os')
 
 const encryptMessage = (name,message,file) => {
-  //
+  var encodedName = Buffer.from(name).toString('base64')
+  var iv = crypto.randomBytes(8).toString('base64');
+  var key = crypto.randomBytes(8);
+  var publicKey = fs.readFileSync(__dirname + '/Keys/' + name + '.pub.pem');
+  var encrypted = crypto.publicEncrypt(publicKey, key);
+  const cipher = crypto.createCipheriv('des-ecb', key, null);
+  let result = cipher.update(message,'utf8','base64');
+  result+= cipher.final('base64')
+  result = result.toString('base64')
+  var ciphertext = encodedName+'.'+iv+'.'+encrypted.toString('base64')+'.'+result;
+  if(typeof file == 'undefined'){
+     console.log(ciphertext);
+  }
+  else{
+    if(file.includes('C:/') || file.includes('c:/')){
+      if(fs.existsSync(file)){
+        console.log('Gabim: File ' + file + ' ekziston paraprakisht');
+      }else{
+        fs.writeFile(file,ciphertext,(err)=>{
+          if(err) throw err;
+          console.log('Mesazhi i enkriptuar u ruajt ne fajllin ' + file);
+        })
+      }
+    }
+    else if(fs.existsSync(os.homedir() + '/' + file)){
+      console.log('Gabim: File ' + file + ' ekziston paraprakisht');
+    }
+    else{
+      fs.writeFile(os.homedir() + '/' + file,ciphertext,(err)=>{
+        if(err) throw err;
+        console.log('Mesazhi i enkriptuar u ruajt ne fajllin ' + file);
+      })
+    }
+  }
 }
 
 const decryptMessage = (message) =>{
-  //
+  if(fs.existsSync(os.homedir()+ '/' + message) || fs.existsSync(message)){
+    if(fs.existsSync(os.homedir()+ '/' + message)){
+      var messageFile = fs.readFileSync(os.homedir()+ '/' + message).toString();
+      var sT = messageFile.split('.')
+      if(sT.length != 4){
+        console.log('Formati i ketij teksti nuk perputhet');
+      }else{
+        decodeMessage(sT);
+      }
+    }
+  }else{
+    var seperateText = message.split('.')
+    if(seperateText.length != 4){
+      console.log('Formati i ketij teksti nuk perputhet');
+    }else{
+      decodeMessage(seperateText);
+    }
+  }
 }
 
 const decodeMessage = (message) =>{
