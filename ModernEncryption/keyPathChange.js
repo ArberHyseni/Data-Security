8ce68a4b9324e1ebb73fd0adeb71ae0061716794
artanvrajolli <artanvrajolli@gmail.com>
2020-06-03 15:06:43 +0200

    added some async and await

--- ModernEncryption/keyPathChange.js
+++ ModernEncryption/keyPathChange.js
@@ -5,7 +5,7 @@ const crypto = require('crypto')
 const {import_file} = require('./getAndSendRequest')
 const {moveprocess,findDir,abortProcess} = require('../lib/utilities')
 
-const exportKey = (visibility,name,file) => {
+const exportKey = async (visibility,name,file) => {
   if(!visibility || !name) abortProcess('Argumentet jovalide')
   if(file) file = file.trim()
   visibility = visibility.trim()
@@ -17,27 +17,26 @@ const exportKey = (visibility,name,file) => {
     if(fs.existsSync(os.homedir() + '/' + file))
       abortProcess('Ekziston nje file tjeter me kete emer ne direktoriumin qe deshironi zhvendosjen e celesit')
     if(file.startsWith('~/'))
-      (!fs.existsSync(os.homedir() + '/' + file.substring(2)) ? moveprocess(__dirname + '/Keys/' + name + ((visibility == 'public') ? '.pub.pem' : '.pem'),os.homedir() + '/' + file.substring(2),visibility) : abortProcess('File already exists on that path'))
+      (!fs.existsSync(os.homedir() + '/' + file.substring(2)) ? await moveprocess(__dirname + '/Keys/' + name + ((visibility == 'public') ? '.pub.pem' : '.pem'),os.homedir() + '/' + file.substring(2),visibility) : abortProcess('File already exists on that path'))
     if(file.includes(':/'))
-      ((!fs.existsSync(file)) ? moveprocess(__dirname + '/Keys/' + name + ((visibility=='private') ? '.pem' : '.pub.pem'), file,visibility) : abortProcess('File already exists on that path'))
-    else moveprocess(__dirname + '/Keys/' + name + ((visibility=='public') ? '.pub.pem' : '.pem'),os.homedir() + '/' + file,visibility)
+      ((!fs.existsSync(file)) ? await moveprocess(__dirname + '/Keys/' + name + ((visibility=='private') ? '.pem' : '.pub.pem'), file,visibility) : abortProcess('File already exists on that path'))
+    else await moveprocess(__dirname + '/Keys/' + name + ((visibility=='public') ? '.pub.pem' : '.pem'),os.homedir() + '/' + file,visibility)
   }
 }
 
-const importKey = (name, file) => {
+const importKey = async (name, file) => {
   if(!name || !file) abortProcess('Argumentet jovalide')
   name = name.trim()
   file = file.trim()
-  if(file.includes(':/')){
+  if(file.includes('http')){ 
+    import_file(name,file)
+  }else if(file.includes(':/')){
     if(!fs.existsSync(file)) abortProcess('File path is not valid')
     let text = readImportedKey(file)
     if(typeof text == 'undefined' || (!text.includes('PUBLIC') && !text.includes('PRIVATE'))) abortProcess('Invalid file')
-    if(text.includes('PUBLIC')) moveprocess(file,__dirname+'/Keys/' + name + '.pub.pem','public')
+    if(text.includes('PUBLIC')) await moveprocess(file,__dirname+'/Keys/' + name + '.pub.pem','public')
     if(text.includes('PRIVATE')){
-      moveprocess(file,__dirname+'/Keys/'+name+'.pem','private')
-      setTimeout(()=>{
-        //
-      },1000)
+      await moveprocess(file,__dirname+'/Keys/'+name+'.pem','private')
       let privateKey = fs.readFileSync(__dirname + '/Keys/' + name + '.pem');
       var derivedPrivateKey = crypto.createPrivateKey({'key': privateKey,'passphrase': 'top secret','cipher': 'aes-256-cbc'})
       var derivedPublicKey = crypto.createPublicKey(derivedPrivateKey).export({'type':'spki','format': 'pem','cipher': 'aes-256-cbc','passphrase':'top secret'});
@@ -47,15 +46,13 @@ const importKey = (name, file) => {
       })
     }
   }
-  if(file.startsWith('http' || 'https')){
-    import_file(name,file)
-  }
+  
   if(fs.existsSync(os.homedir()+'/' + file)){
     let text = readImportedKey(os.homedir()+'/' + file)
     if(!text || (!text.includes('PUBLIC') && !text.includes('PRIVATE'))) abortProcess('Invalid file')
-    if(text.includes('PUBLIC')) moveprocess(os.homedir()+'/' + file,__dirname+'/Keys/'+name+'.pub.pem','public')
+    if(text.includes('PUBLIC')) await moveprocess(os.homedir()+'/' + file,__dirname+'/Keys/'+name+'.pub.pem','public')
     if(text.includes('PRIVATE')){
-      moveprocess(os.homedir()+'/'+file,__dirname+'/Keys/'+name+'.pem','private')
+      await moveprocess(os.homedir()+'/'+file,__dirname+'/Keys/'+name+'.pem','private')
       setTimeout(()=>{
         //
       },1000)
@@ -70,12 +67,6 @@ const importKey = (name, file) => {
   }
 }
 
-/*var log = console.log;
-console.log = function() {
-    log.apply(console, arguments);
-    // Print the stack trace
-    console.trace();
-};*/
 
 let readKey = (name,visibility) => {
   if(!fs.existsSync(__dirname + '/Keys/' + name + ((visibility=='public') ? '.pub.pem' : '.pem'))){
