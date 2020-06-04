deec9c6d81483b7ef1ba995867aa9d43c973b10b
engj3ll <arberpr6@gmail.com>
2020-06-04 23:14:02 +0200

    sender option revised

--- ModernEncryption/sendEncryptedMessage.js
+++ ModernEncryption/sendEncryptedMessage.js
@@ -2,31 +2,42 @@ const crypto = require('crypto')
 const path = require('path')
 const fs = require('fs')
 const os = require('os')
-const jwt = require('jsonwebtoken');
+const {abortProcess} = require('../lib/utilities')
+const jwt = require('jsonwebtoken')
 
 const encryptMessage = (name,message,file,token) => {
   if(file == '--sender') file = null;
-
-  var encodedName = Buffer.from(name).toString('base64')
-  var iv = crypto.randomBytes(8).toString('base64');
-  var key = crypto.randomBytes(8);
-
-
-  var privateKEY = fs.readFileSync(__dirname + '/Keys/' + name + '.pem');
-  var publicKey = fs.readFileSync(__dirname + '/Keys/' + name + '.pub.pem');
- 
-
-  var encrypted = crypto.publicEncrypt(publicKey, key);
-  const cipher = crypto.createCipheriv('des-ecb', key, null);
-  let result = cipher.update(message,'utf8','base64');
-  result+= cipher.final('base64')
+  var encodedName = Buffer.from(name).toString('base64') //base64(utf8(<name>))
+  var iv = crypto.randomBytes(8).toString('base64') // base64(<iv>)
+  var key = crypto.randomBytes(8) //key
+  
+  var publicKey = fs.readFileSync(__dirname + '/Keys/' + name + '.pub.pem') //publicKey
+  var encrypted = crypto.publicEncrypt(publicKey, key)
+  const cipher = crypto.createCipheriv('des-ecb', key, null) //rsa(<key>)
+  let result = cipher.update(message,'utf8','base64') // base64(rsa(<key>))
+  result += cipher.final('base64')
   result = result.toString('base64')
-  var ciphertext = encodedName+'.'+iv+'.'+encrypted.toString('base64')+'.'+result;
-  if(typeof file == 'undefined' || file == null){
-     token = process.argv[6];
-     
 
-     console.log(ciphertext);
+  var allTokens = fs.readFileSync(path.join(__dirname,'../Core/Token/CoreToken.rtf'))
+  if(allTokens==null || typeof allTokens == 'undefined') abortProcess('No Tokens Found')
+  var tokenArray = allTokens.split('|')
+  var senderName
+  tokenArray.forEach((element,index)=>{
+    if(element===token) senderName = element[index+1]
+  })
+  var privateKey = fs.readFileSync(__dirname + '/Keys/' + senderName + '.pem') //privateKey
+  var encryptedSender = Buffer.from(senderName).toString('base64') //base64(utf8(<sender>))
+  var signedMessage = crypto.sign('des-ecb', message, privateKey).update('base64').final('base64').toString('base64') //. base64(signature(des(<message>)))
+
+  var ciphertext = encodedName+'.'+iv+'.'+encrypted.toString('base64')+'.'+result
+
+  if(typeof file == 'undefined' || file == null){
+     token = process.argv[6]
+     if(token=='undefined') console.log(ciphertext)
+     else{
+      ciphertext += encryptedSender+'.'+signedMessage
+      console.log(ciphertext)
+     }
   }
   else{
     if(file.includes('C:/') || file.includes('c:/')){
